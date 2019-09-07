d8a114dadb3913b1bd396a62797604b4f0a87cb6
DavidKu <ts01561526@gmail.com>
2019-09-06 21:57:54 -0400

    Update AnimationSpyder.py

--- AnimationSpyder.py
+++ AnimationSpyder.py
@@ -9,8 +9,10 @@ Created on Thu Sep  5 21:41:28 2019
 # In[1]:
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from mpl_toolkits.mplot3d import Axes3D
 plt.style.use("ggplot")
 
 # In[2]:
@@ -79,6 +81,92 @@ y_noise = y_noise - y_noise.mean()
 # Plotting the data
 plt.figure(figsize = (10, 6))
 plt.scatter(x, y_noise, facecolors = "none", edgecolor = "darkblue", label = "sine + noise")
+plt.title("Noisy sine function, ploynomial ridge regression fit")
+plt.xlabel("x")
+plt.ylabel("y")
 
+# Design matrix for polynomial regression of order 2
+X = np.vstack((x, x**2)).T
 
+# Calculating closed form regression coefficients with lambda = 0 and 25
+theta_calc_0 = closed_form_reg_solution(X, y_noise, lamda = 0)
+theta_calc_25 = closed_form_reg_solution(X,y_noise,lamda = 25)
 
+# Plot 
+plt.plot(x,X@theta_calc_0, '--', label = 'Ridge fit lambda = {}'.format(0))
+plt.plot(x,X@theta_calc_25, '--', label = 'Ridge fit lambda= {}'.format(25))
+plt.legend()
+plt.show()
+
+print('Coefficients with lambda = 25 are: ' ,theta_calc_25)
+
+# In[]:
+"Gradient Descent"
+GDescent = gradient_descent_reg(X, y_noise, np.array([0, -5]).reshape(2, 1), alpha = 0.05, lamda = 25, num_iters = 1000)
+GradientTheta = GDescent[0]
+J_history_reg = GDescent[1]
+theta_0 = GDescent[2]
+theta_1 = GDescent[3]
+
+# In[6]:
+# Creat data for contour plot
+
+l = 25 # Complexity hyperparameter lambda = 25
+
+# Setup of meshgrid o theta values
+T1, T2 = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-6, 3, 100))
+
+# Computing the cost funtion for each theta combination
+zs = np.array(  [costFunction(X, y_noise.reshape(-1,1),np.array([t1,t2]).reshape(-1,1),l) 
+                     for t1, t2 in zip(np.ravel(T1), np.ravel(T2)) ] )
+
+# Reshape the cost value
+Z = zs.reshape(T1.shape)
+
+# In[7]:
+
+# plot the contour
+fig1, ax1 = plt.subplots(figsize = (7, 7))
+ax1.contour(T1, T2, Z, 100, cmap = "jet")
+plt.show()
+# Create animation
+line, = ax1.plot([], [], "r", label = "Gradient Descent", lw = 1.5)
+point, = ax1.plot([], [], "*", color = "red", markersize = 4)
+value_display = ax1.text(0.02, 0.02, " ", transform = ax1.transAxes)
+
+def init_1():
+    line.set_data([], [])
+    point.set_data([], [])
+    value_display.set_text(" ")
+    
+    return line, point, value_display
+
+def animate_1(i):
+    # Animate line
+    line.set_data(theta_0[:i], theta_1[:i])
+    
+    # Animate points
+    point.set_data(theta_0[i], theta_1[i])
+    
+    # Animate value display
+    value_display.set_text("Min = " + str(J_history_reg[i]))
+    
+    return line, point, value_display
+
+ax1.legend(loc = 1)
+
+anim1 = FuncAnimation(fig1, animate_1, init_func = init_1, frames = len(theta_0), interval = 100, repeat_delay = 60, blit = True)
+
+plt.show()
+# In[test]:
+#Writer = matplotlib.animation.writers["ffmped"]
+#writer = Writer(fps = 30, metadata = dict(artist = "David"), bitrate = 1800)
+#anim1.save("/Users/dauku/Desktop/Python/Visualization/VisualizationExamples/Contour.mp4", writer = writer)
+
+# In[8]:
+"""3-D plot"""
+fig2 = plt.figure(figsize = (7, 7))
+ax2 = Axes3D(fig2)
+
+# Surface plot
+ax2.plot_surface(T1, T2, X, rstride = 5, cstride = 5, cmap = "jet", alpha = 0.5)
